242ec398eb25e5f653212cb4e440788103489d17
exp-3 <takaaki.service@gmail.com>
2017-01-06 05:33:48 +0900

    dmaを用いたpwmを同期し，ズレが20us以内に収まることを確認した．

--- rpi/main.c
+++ rpi/main.c
@@ -6,32 +6,59 @@
 volatile int beep_flag = 0;
 volatile unsigned long long int stop_time = 0;
 
-void beep() {
-  unsigned long long int temp_time = syst_micros();
+#define N 32
+static uint32_t data[N];
+
+// void beep() {
+//   unsigned long long int temp_time = syst_micros();
+//   pwm_start();
+//   // printf("b\r\n");
+//   // unsigned long long int temp_stop_time = temp_time + BEEP_DURATION;
+//   // stop_time = temp_stop_time;
+//   stop_time = temp_time + BEEP_DURATION;
+//   beep_flag = 1;
+//   // printf("c\r\n");
+// }
+
+unsigned int cnt_syncro = 0;
+unsigned int cnt_toggle = 0;
+
+void syncro() {
   pwm_start();
-  // printf("b\r\n");
-  // unsigned long long int temp_stop_time = temp_time + BEEP_DURATION;
-  // stop_time = temp_stop_time;
-  stop_time = temp_time + BEEP_DURATION;
-  beep_flag = 1;
-  // printf("c\r\n");
+  // printf("hoge\r\n");
+  cnt_syncro++;
 }
 
 void toggle_pin() {
   static int state = 0;
-  gpio_write(9, state);
   state ^= 1;
+  cnt_toggle++;
+  if(!state) {
+    pwm_stop();
+  }
+
+  gpio_write(9, state);
+  // printf("huga\r\n");
 }
 
 int main(void) {
   rpi_init();
 
   gpio_set_pin_mode(9, GPIO_OUTPUT);
-  // synchronize_attach_interrupt(beep);
-  // timer_attach_interrupt(toggle_pin);
-  //
-  // interrupt_enable_IRQ();
-  pwm_start();
+
+  for(int i = 0; i < N; i++) {
+    data[i] = 0x80000000;
+  }
+  pwm_set_data(data, N);
+
+  timer_set_period(500000);
+
+  synchronize_attach_interrupt(syncro);
+  timer_attach_interrupt(toggle_pin);
+
+  interrupt_enable_IRQ();
+
+  // pwm_start();
 
   while(1) {
     // if(beep_flag) {
@@ -46,8 +73,12 @@ int main(void) {
     //   printf("dest[%02d]=0x%08x\r\n", i, dest_ary[i]);
     // }
 
-    pwm_debug_info();
-    pwm_rescue();
+    // pwm_start();
+    asm("nop");
+    printf("syncro: %d, toggle: %d\r\n", cnt_syncro, cnt_toggle);
+    // printf("data address is 0x%08x\r\n", data);
+    // pwm_debug_info();
+    // pwm_rescue();
   }
   return 0;
 }
