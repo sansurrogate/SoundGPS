9e786f7a80bb216d70f97293f66dc27de7d387e8
exp-3 <takaaki.service@gmail.com>
2017-01-13 20:57:44 +0900

    1023bitの嵩系列により，動作してるっぽいことを確認した

--- research/psk/multi_channel/code_generator.py
+++ research/psk/multi_channel/code_generator.py
@@ -56,9 +56,9 @@ if __name__ == '__main__':
     golds = generate_gold(ms[0], ms[1])
     kasamis = generate_kasami(n, ms[0])
     # codes = golds[:3]
-    codes = kasamis[:3]
+    codes = kasamis[:4]
+    print(codes)
     corr_c = correlate_calculator(codes)
-    corr = corr_c.get_correlate()
-    print(corr)
-    # corr_c.plot_self_correlate()
+    # corr = corr_c.get_correlate()
+    # print(corr)
     corr_c.plot_correlate()
